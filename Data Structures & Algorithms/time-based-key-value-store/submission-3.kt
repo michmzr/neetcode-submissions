f9e2c6905620443900a86f4e80f2420b7ab86b4d
Mike <michmzr@gmail.com>
2026-07-15 16:50:09 +0200

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.kt
@@ -0,0 +1,15 @@
+class TimeMap() {
+
+    val map = HashMap<String, java.util.TreeMap<Int, String>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        map.putIfAbsent(key, TreeMap<Int, String>())
+        map.get(key)!!.put(timestamp, value)
+    }
+
+     fun get(key: String, timestamp: Int): String? {
+        println(map.get(key))
+        val floor =map.get(key)?.floorEntry(timestamp)
+        return if(floor != null) floor.value else ""
+    }
+}
