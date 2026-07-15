e3aedbd2f8e529d99a461798f03d22552af0b040
Mike <michmzr@gmail.com>
2026-07-15 15:03:25 +0200

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.kt
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+     fun printNodes(l1: ListNode?) {
+        var pos = l1
+
+        while (pos != null) {
+            print(pos.`val`)
+            if (pos.next != null) {
+                print("->")
+            }
+            pos = pos.next
+        }
+
+        println()
+    }
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var dummy: ListNode = ListNode(-1)
+        var current: ListNode = dummy
+
+        var pos1 = l1
+        var pos2 = l2
+
+        var nrPos = 0
+        var carry=0
+
+        while (pos1 != null || pos2 != null || carry != 0) {
+            val sum =  (pos1?.`val` ?: 0) + (pos2?.`val` ?: 0) + carry
+
+            carry = sum / 10
+
+           // println("$nrPos -> pos1: ${pos1?.`val`}, pos2: ${pos2?.`val`}, sum: $sum carry: $carry, pos1.next: ${pos1?.next?.`val`}, pos2.next: ${pos2?.next?.`val`}")
+
+            current.next = ListNode(sum % 10)
+            current = current.next!!
+
+            pos1 = pos1?.next
+            pos2 = pos2?.next
+
+            nrPos++
+        }
+
+        return dummy.next
+    }
+}
