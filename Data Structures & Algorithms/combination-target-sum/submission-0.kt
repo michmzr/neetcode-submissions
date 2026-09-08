d15518494224205ce0f9466a4a3094f7706745b8
Mike <michmzr@gmail.com>
2026-09-08 16:28:13 +0200

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun combinationSum(nums: IntArray, target: Int): List<List<Int>> {
+        nums.sort()
+
+        val res = mutableListOf<List<Int>>()
+
+        fun dfs(i: Int, cur: MutableList<Int>, total: Int) {
+            if(total == target) {
+                res.add(ArrayList(cur))
+                return
+            }
+
+            if(i >= nums.size || total > target) {
+                return
+            }
+
+            cur.add(nums[i])
+            dfs(i, cur, total + nums[i])
+            cur.removeAt(cur.size -1) //do a backstep
+            dfs(i+1, cur, total) // try with next number
+        }
+
+        dfs(0, mutableListOf(), 0)
+
+        return res
+    }
+}
