536b275e800957b13ea08a33849536678de2975f
Mike <michmzr@gmail.com>
2026-09-08 10:43:11 +0200

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.kt
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+      fun goodNodes(root: TreeNode?): Int {
+        //Jak pozyskac informacje, ktore nody sa wieksze dla aktualnej liczby
+        require(root != null)
+
+        return traverse(root, root.`val`) +1
+    }
+
+    fun traverse(node: TreeNode, maxValue: Int): Int {
+        val nodeValue = node.`val`
+
+        var found = 0
+
+        if(maxValue < nodeValue)
+            found++
+
+        if(node.left != null) {
+            found += traverse(node.left!!, max(nodeValue, maxValue))
+        }
+
+        if(node.right != null) {
+            found += traverse(node.right!!, max(nodeValue, maxValue))
+        }
+
+        return found
+    }
+}
